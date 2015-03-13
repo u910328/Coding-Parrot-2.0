e36810c986cd79b574512d0c025ad66afcee74dd
u910328 <u910328@gmail.com>
2015-03-13 10:19:18 -0500

    remove fbRW and model RW, merge it with localFb and model

--- coreServices/model.js
+++ coreServices/model.js
@@ -1,13 +1,45 @@
 angular.module('core', ['firebase', 'myApp.config'])
-    .factory('model', function (config, fbutil, $q) {
-        return {
-            value:{},
-            db:{
-                main:{online:[]},
-                A:{online:[]},
-                B:{online:[]},
-                C:{online:[]},
-                D:{online:[]}
+    .factory('model', function (config, fbutil, $q, snippet, viewLogic) {
+        var model={
+            update:update,
+            updateView:updateView
+        };
+
+        function checkRow(i, whichCol) {
+            var rowI = viewLogic["table"][i],
+                rowTitle = viewLogic["table"][0],
+                match = true;
+            if (!eval("model." + rowTitle[whichCol] + rowI[whichCol])) {
+                return false;
+            }
+            for (var j = 0; j < rowTitle.length - 2; j++) {
+                if (rowI[j] && j!= whichCol) {
+                    if (!eval("model." + rowTitle[j] + rowI[j])) {
+                        match = false;
+                        break
+                    }
+                }
+            }
+            return match;
+        }
+//TODO: 變成兩部分 1.先計算所有變動 2.將變動套用到view上
+        function updateView(path) {
+            var renew = viewLogic["renew"][path];
+            for (var i = 1; i < renew.length; i++) {
+                if(checkRow(i, renew[0])){
+                    var renewValue=renew[(renew.length-1)];
+                    for(var key in renewValue){
+                        eval("model.view"+key+"=renewValue[key]")
+                    }
+                }
             }
         }
+
+        function update(path, value) {
+            if(value){
+                eval("model.view"+path+"=value");
+            }
+            updateView(path)
+        }
+
     });
