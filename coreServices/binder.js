810670594095804708ac1c59db8a9847ec4fe83b
u910328 <u910328@gmail.com>
2015-03-19 12:22:44 -0500

    rewrite updater

--- coreServices/binder.js
+++ coreServices/binder.js
@@ -11,39 +11,45 @@ angular.module('core', ['firebase', 'myApp.config'])
 
         function bindUpdator(scope, modelPath, rule){
             var modelPathArr=("model."+modelPath).split("."),
-                key=modelPathArr[modelPathArr.length-1], updator;
+                key=modelPathArr[modelPathArr.length-1], updater;
             switch(rule.type){
                 case "simplePagination":
                     //snippet.evalAsignment(["scope", "updateVar"+key], {currentPage:1});
-                    updator=function(prevOrNext){
+                    updater=function(page){
                         var orderBy="orderBy"+rule.orderBy[0]+"("+rule.orderBy[1]+")",
-                            currentPage=scope["updateVar"+key]["currentPage"],
-                            query=orderBy||"orderByKey()"+".startAt("+scope["updateVar"+key]["lastItem"][currentPage]+").limitToFirst("+rule.itemPerPage+")",
+                            currentPage=scope[key+"Var"]["currentPage"],
+                            query=orderBy||"orderByKey()"+".startAt("+scope[key+"Var"]["lastItem"][currentPage]+").limitToFirst("+rule.itemPerPage+")",
                             sPaginationRule={query:query, sync:false, eventType:'child_added'};
-                        localFb.load(rule.fbPath, modelPath, sPaginationRule);
-                        if(!scope["updateVar"+key]["lastItem"][Number(prevOrNext)+currentPage]){
-                            localFb.load(rule.fbPath, modelPath, sPaginationRule);
+                        if(!scope["updateVar"+key]["lastItem"][page]){
+                            var restItem=rule.itemPerPage;
+                            localFb.load(rule.fbPath, modelPath, sPaginationRule, function(snap, prevChildName){
+                                restItem--;
+                                if(restItem===0){
+                                    updateCurrentPage(scope, key, modelPath);
+                                }
+                            });
+                        } else{
+                            updateCurrentPage(scope, key, modelPath); //將資料處理好存到scope[key]並更新currentPage, lastItem等
                         }
-                        genCurrentPage(scope,key, modelPath); //將資料處理好存到scope[key]
                     };
                     break;
                 case "pagination":
-                    updator=function(query){
+                    updater=function(query){
                         localFb.load(rule.fbPath, modelPath, query);
                     };
                     break;
                 case "infiniteScroll":
-                    updator=function(query){
+                    updater=function(query){
                         localFb.load(rule.fbPath, modelPath, query);
                     };
                     break;
                 default:
-                    updator=function(query){
+                    updater=function(query){
                             localFb.load(rule.fbPath, modelPath, query);
                         };
                     break;
             }
-            snippet.evalAsignment(["scope", "update"+key], updator);
+            snippet.evalAsignment(["scope", "update"+key], updater);
         }
 
         function bindScope(scope, rulePath){
